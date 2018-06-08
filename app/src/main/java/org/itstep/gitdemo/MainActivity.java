6f318b0eb8d5ece9c69007c9bc3aed5af13a4fa3
Maxim Shaptala <maxshaptala@gmail.com>
2018-06-08 19:13:54 +0300

    В главную активность добавлена кнопка и обработчик события клика по ней

--- app/src/main/java/org/itstep/gitdemo/MainActivity.java
+++ app/src/main/java/org/itstep/gitdemo/MainActivity.java
@@ -2,6 +2,7 @@ package org.itstep.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
